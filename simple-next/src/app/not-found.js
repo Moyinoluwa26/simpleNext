a24e264cd5505e708dacb3b0959adfe09ff1ecfb
Oyejide <victormoyin435@gmail.com>
2024-08-28 22:44:23 +0100

    This end

--- simple-next/src/app/not-found.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function notFound() {
-    return (
-        <div> This is the page for Not Found Resources ehn </div>
-    )
-}
--- simple-next/src/app/user/register/page.js
+++
@@ -1,5 +0,0 @@
-import Register from "@/app/components/register/page";
-
-export default function RegisterPage() {
-    return <Register />;
-}
